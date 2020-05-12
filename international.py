1302a35a06e40e841764481e2ec1bc3e7c7c1c5b
Fabien Maussion <fabien.maussion@uibk.ac.at>
2020-05-12 18:54:55 +0200

    add internationalization file

---
+++ international.py
@@ -0,0 +1,123 @@
+supported_languages = ['en', 'de', 'fr']
+
+trads = {
+    'clear_button':
+        {
+            'en': 'Clear selection',
+            'de': 'Auswahl aufheben',
+            'fr': 'Annuler la sélection',
+        },
+    'bar_glaciers_selected':
+        {
+            'en': 'Glaciers selected: \n {} of {}',
+            'de': 'Ausgewählte Gletscher: \n {} von {}',
+            'fr': 'Glaciers sélectionnés: \n {} de {}',
+        },
+    'bar_area':
+        {
+            'en': 'Area',
+            'de': 'Fläche',
+            'fr': 'Surface',
+        },
+    'bar_volume':
+        {
+            'en': 'Volume',
+            'de': 'Volumen',
+            'fr': 'Volume',
+        },
+    'bar_sealevel_text':
+        {
+            'en': 'Sea-level equivalent: ',
+            'de': 'Beitrag zum Meeresspiegelanstieg:<br>',
+            'fr': "Élévation du niveau de la mer:<br>",
+        },
+    'bar_sealevel_y':
+        {
+            'en': 'Volume in mm sea-level equivalent',
+            'de': 'Beitrag zum Meeresspiegelanstieg in mm',
+            'de': 'Élévation du niveau de la mer en mm',
+        },
+    'map_plot_x':
+        {
+            'en': 'Longitude',
+            'de': 'Geographische Länge',
+            'fr': 'Longitude',
+        },
+    'map_plot_y':
+        {
+            'en': 'Latitude',
+            'de': 'Geographische Breite',
+            'fr': 'Latitude',
+        },
+    'temp_plot_x':
+        {
+            'en': 'Annual Temperature at avg. altitude (°C)',
+            'de': 'Jahresdurchschnittstemperatur in mittlerer Höhe (°C)',
+            'fr': "Temperature annuelle à l'altitude moyenne (°C)",
+        },
+    'trend_plot_x':
+        {
+            'en': 'Temperature trend 1979-2018 (°C/decade)',
+            'de': 'Temperaturtrend 1979-2018 (°C/Dekade)',
+            'fr': 'Tendance de température 1979-2018 (°C/décade)',
+        },
+    'trend_plot_y':
+        {
+            'en': 'Glacier count',
+            'de': 'Gletscher Anzahl',
+            'fr': 'Nombre de glaciers',
+        },
+    'precip_plot_x':
+        {
+            'en': 'Annual Precipitation (mm/yr)',
+            'de': 'Jährlicher Niederschlag (mm/Jahr)',
+            'fr': 'Précipitations annuelles (mm/an)',
+        },
+    'elev_plot_x':
+        {
+            'en': 'Mean elevation of the glacier (m a.s.l.)',
+            'de': 'Mittlere Höhe des Gletschers (m ü.M.)',
+            'fr': 'Altitude moyenne du glacier (m)',
+        },
+    'elev_plot_y':
+        {
+            'en': 'Latitude',
+            'de': 'Geographische Breite',
+            'fr': 'Latitude',
+        },
+    'instructions':
+        {
+            'en': 'Choose your region of interest by clicking and dragging '
+                  'the mouse in the map or in the other figures.<br><b>Reset '
+                  'your selection with the "Clear selection"  button on the '
+                  'left</b>.<br>For more information about the app and our '
+                  'data sources, visit <a href="http://edu.oggm.org/en/latest/'
+                  'explorer.html">edu.oggm.org</a>',
+            'de': 'Wähle eine Region aus, indem du mit der Maus in der '
+                  'Weltkarte oder in den anderen Graphiken klickst und ziehst. '
+                  '<br><b>Setze deine Auswahl mit dem "Auswahl aufheben"-'
+                  'Button auf der linken Seite zurück</b>. Für mehr '
+                  'Informationen zu der App und den Datenquellen gehe auf '
+                  '<a href="http://edu.oggm.org/en/latest/explorer.html">'
+                  'edu.oggm.org</a>.',
+            'fr': 'Choisis une région en cliquant et sélectionnant un rectangle '
+                  'sur la carte ou sur les autres figures. '
+                  '<br><b>Annule la sélection avec le bouton sur la gauche</b>. '
+                  "Pour plus d'informations sur l'appliquation et son utilisation, "
+                  'visite <a href="http://edu.oggm.org/en/latest/explorer.html">'
+                  'edu.oggm.org</a>.',
+         },
+    'abbreviations':
+        {
+            'en': 'Abbreviations:<br>'
+                  '"asl": above sea level, total glacier volume that is above sea level.<br>'
+                  '"bsl": below sea level, total glacier volume that is below sea level in the ocean (grounded).',
+            'de': 'Abkürzungen:<br>'
+                  '"asl" (above sea level): diese Rubrik umfasst das Gletschervolumen, das sich oberhalb des Meeresspiegels befindet. <br>'
+                  '"bsl" (below sea level): diese Rubrik umfasst das Gletschervolumen, das sich unterhalb des Meerespiegels befindet, also bis auf den Meeresboden reicht.',
+            'fr': 'Abbréviations:<br>'
+                  '"asl": above sea level, volume total de glace se trouvant au dessus du niveau de la mer.<br>'
+                  '"bsl": below sea level, volume total de glace se trouvant en dessous du niveau de la mer.',
+        },
+
+}
